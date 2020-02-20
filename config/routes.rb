355641f687da485a330069b463cc44739217adce
Tan Nguyen <tan.nguyen@employmenthero.com>
2020-02-20 11:21:50 +0700

    refactor routes and add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
 
   resources :microposts
   resources :users
 
+  get 'sessions/new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
