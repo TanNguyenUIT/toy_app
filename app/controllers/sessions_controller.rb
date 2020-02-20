e4cf4b456cbb696a552af5e0cd3fc15d8a57a389
Tan Nguyen <tan.nguyen@employmenthero.com>
2020-02-20 10:44:12 +0700

    handle log in and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,27 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
   def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email].downcase )
+    if user && user.authenticate(session_params[:password])
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = "Invalid email/password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+  private
+
+  def session_params
+    params[:session]
   end
 end
